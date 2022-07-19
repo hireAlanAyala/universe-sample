f8d6b5a449c874409f8fbfdfd45565a908c2a0ef
Alan Ayala <kanonbolt128@gmail.com>
2022-07-19 17:09:01 -0500

    Fix: user link not working ona  post

--- src/components/PostBlock/PostBlock.js
+++ src/components/PostBlock/PostBlock.js
@@ -1,20 +1,17 @@
 import React from 'react'
 import styles from './styles.module.css';
-import { useNavigate, Link } from "react-router-dom";
+import { useHistory, Link } from "react-router-dom";
 
-const PostBlock = ({ title, body, userId }) => {
-  const navigate = useNavigate();
-  const routeToPost = () => navigate(`/post/${userId}`, { state: { title, body, userId }})
-
-  return (
-    <div onClick={routeToPost} className={styles.wrapper}>
-      <Link to={`/user/${userId}`} state={{ userId }} className={styles.userLink}>
-        <p>User #{userId}</p>
-      </Link>
-      <p className={styles.title}>{title}</p>
-      <p>{body}</p>
-    </div>
+const PostBlock = ({ title, body, userId }) => (
+    <Link to={`/post/${userId}`} state={{ title, body, userId }}>
+        <div className={styles.wrapper}>
+          <Link to={`/user/${userId}`} state={{ userId }} className={styles.userLink}>
+            <p>User #{userId}</p>
+          </Link>
+          <p className={styles.title}>{title}</p>
+          <p>{body}</p>
+        </div>
+    </Link>
   );
-}
 
 export default PostBlock
