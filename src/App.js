13126884610b6b622b78b9ac90a52c39210f4a09
Alan Ayala <kanonbolt128@gmail.com>
2022-07-21 13:57:47 -0500

    Feature: added code splitting on routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
+import { Suspense, lazy  } from 'react';
 import { Routes, Route, Navigate, Link } from "react-router-dom";
-import Feed from './containers/Feed';
-import PostInfo from './containers/PostInfo';
-import User from './containers/User';
 import Logo from './assets/logo.png';
+
+/* Lazy loads route containers to save on bundle size */
+const Feed = lazy(() => import('./containers/Feed'));
+const PostInfo = lazy(() => import('./containers/PostInfo'));
+const User = lazy(() => import('./containers/User'));
+
+/*
+  UPGRADES:
+  - In the feed page use a pop up to display the posts instead of routing to a different page. To make the experience smoother.
+  - Consider caching the data so that on load the app uses the cached data until it can complete the api request.
+  - When the appliation grows in components I would use something like storybook to document them for other devs.
+  - Add an error boundary in case the network fails to load
+  - Make sure that we stay on top of dynamic import opportunities to manage bundle size. Import logic only when requirements are met and it's needed.
+  - It might be worth to consider using preact to save on bundle size and next.js to make content more accessible to bots and improve SEO
+*/
 
 const App = () => (
   <>
@@ -12,12 +25,14 @@ const App = () => (
       </Link>
     </header>
     <div className="app">
-      <Routes>
-        <Route path="/" element={<Feed />} />
-        <Route path="post/:userId" element={<PostInfo />} />
-        <Route path="user/:userId" element={<User />} />
-        <Route path="*" element={<Navigate to="/" replace />} />
-      </Routes>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path="/" element={<Feed />} />
+          <Route path="post/:userId" element={<PostInfo />} />
+          <Route path="user/:userId" element={<User />} />
+          <Route path="*" element={<Navigate to="/" replace />} />
+        </Routes>
+      </Suspense>
     </div>
   </>
 );
